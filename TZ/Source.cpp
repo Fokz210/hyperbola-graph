324679d633f58e179f99b6b85798d6159429af9d
Fokz210 <cheremisenov96@gmail.com>
2020-05-02 18:55:29 +0300

    added ellipse class

--- TZ/Source.cpp
+++ TZ/Source.cpp
@@ -76,23 +76,48 @@ protected:
 class ellipse
 {
 public:
-	ellipse (float radius_x, float radius_y) :
+	ellipse (float radius_x, float radius_y, float offset_x = 400.f, float offset_y = 300.f, float polygons = 100.f) :
+		polygons_ (polygons),
 		radius_x_ (radius_x),
 		radius_y_ (radius_y),
-		shape_ ()
+		offset_x_ (offset_x),
+		offset_y_ (offset_y),
+		shape_ (polygons)
 	{
-
+		//shape_.setClosed (true);
 	}
 
 	void update ()
 	{
-		shape_.setRadius (radius_x_);
-		shape_.setScale (sf::Vector2f (1, radius_y_ / radius_x_));
+		shape_.removeVertices (0, polygons_);
+
+		for (float angle = 0; angle < 3.1415f * 2; angle += 3.1415f * 2.f / polygons_)
+			shape_.addVertex (sf::Vector2f (offset_x_ + cosf (angle) * radius_x_, offset_y_ +  sinf (angle) * radius_y_));
+		shape_.update ();
 	}
 
+	void render (sf::RenderTarget & target)
+	{
+		target.draw (shape_);
+	}
+
+	void set_radius_x (float radius_x) { radius_x_ = radius_x; }
+	void set_radius_y (float radius_y) { radius_y_ = radius_y; }
+	void set_position (sf::Vector2f position) { offset_x_ = position.x; offset_y_ = position.y; }
+	void set_color (sf::Color color) { shape_.setColor (color); }
+	void set_thickness (float thickness) { shape_.setThickness (thickness); }
+
+	float get_radius_x () { return radius_x_; }
+	float get_radius_y () { return radius_y_; }
+	sf::Vector2f get_position () { return sf::Vector2f (offset_x_, offset_y_); }
+	sf::Color get_color () { return shape_.getColor (); }
+	float get_thickness () { return shape_.getThickness (); }
+
 protected:
+	float polygons_;
 	float radius_x_, radius_y_;
-	sf::CircleShape shape_;
+	float offset_x_, offset_y_;
+	sw::Spline shape_;
 };
 
 int main ()
@@ -100,10 +125,21 @@ int main ()
 	sf::RenderWindow window (sf::VideoMode (800, 600), "Test");
 
 	hyperbola hyp (1, 1);
+	hyp.set_color (sf::Color::Blue);
+	hyp.set_thickness (5.f);
 	hyp.update ();
 
+	ellipse el (200, 100);
+	el.set_color (sf::Color::Black);
+	el.set_position (sf::Vector2f (400, 300));
+	el.set_thickness (5.f);
+
+	el.update ();
+	
 	while (window.isOpen ())
 	{
+
+		window.clear (sf::Color::White);
 		sf::Event event;
 		while (window.pollEvent (event))
 		{
@@ -116,6 +152,7 @@ int main ()
 
 
 		hyp.render (window);
+		el.render (window);
 		window.display ();
 	}
 
